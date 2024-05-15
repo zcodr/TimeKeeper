111c2f9d318691eadc74bee9d7a31ce7ffba9a9a
zcodr <greninja1358@gmail.com>
2024-05-14 19:06:14 -0700

    ldf

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -3,6 +3,7 @@ extends Node
 var tasks : Dictionary
 
 func _ready():
-	for i in range(365):
+	tasks[Time.get_date_string_from_system()] = []
+	for i in range(100):
 		var unix_time = Time.get_unix_time_from_system() + (86400 * i)
 		tasks[Time.get_date_string_from_unix_time(unix_time)] = []
